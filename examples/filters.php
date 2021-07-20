f8de69fe225ef37c0444b63dfc01b1d3eb0cc36e
Laurent Laville <laurent.laville@gmail.com>
2021-07-20 05:23:06 +0000

    add reference to an advanced filter usage

---
+++ examples/filters.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+/**
+ * Wants to see more: Have a look on how to filter phpunit test results with following files
+ * - https://github.com/llaville/phpunit-LoggerTestListener/blob/2.0.0/examples/phpunit.monolog.xml
+ * - https://github.com/llaville/phpunit-LoggerTestListener/blob/2.0.0/examples/bootstrap.monolog.php
+ *
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php BSD License
+ * @since    Example available since Release 2.0.0
+ */
